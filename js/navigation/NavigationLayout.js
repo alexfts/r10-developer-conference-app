2471dd959ac7aad5492dd64be94c95833e8e5b76
alexfts <alxfetisova@gmail.com>
2019-02-26 11:00:20 -0800

    Add tab nav icons

--- js/navigation/NavigationLayout.js
+++ js/navigation/NavigationLayout.js
@@ -8,6 +8,7 @@ import FavesScreen from '../screens/Faves';
 import ScheduleScreen from '../screens/Schedule';
 import SessionScreen from '../screens/Session';
 import MapScreen from '../screens/Map';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const AboutStack = createStackNavigator({
   About: AboutScreen
@@ -24,10 +25,44 @@ const MapStack = createStackNavigator({
   Map: MapScreen
 });
 
-// Dedicated stacks for Schedule and Faves will go here too!
-export default createBottomTabNavigator({
-  About: AboutStack,
-  Schedule: ScheduleStack,
-  Faves: FavesStack,
-  Map: MapStack
-});
+export default createBottomTabNavigator(
+  {
+    About: AboutStack,
+    Schedule: ScheduleStack,
+    Faves: FavesStack,
+    Map: MapStack
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'About') {
+          iconName = `ios-calendar`;
+          // Sometimes we want to add badges to some icons.
+          // You can check the implementation below.
+        } else if (routeName === 'Map') {
+          iconName = `ios-map`;
+        } else if (routeName === 'Faves') {
+          iconName = `ios-heart`;
+        } else {
+          // schedule
+          iconName = 'ios-information-circle';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: '#fff',
+      inactiveTintColor: '#999999',
+      labelStyle: {
+        fontSize: 10
+      },
+      style: {
+        backgroundColor: 'black'
+      }
+    }
+  }
+);
