f43c0d2d674a6e1aea8af2357a25d3358b554ab4
alexfts <alxfetisova@gmail.com>
2019-02-28 14:53:37 -0800

    Implement Map screen

--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { Text, View, FlatList } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import styles from './styles';
 
 export default ({ data }) => {
   return (
-    <View>
-      <Text>Map</Text>
-    </View>
+    <MapView
+      style={{ ...styles.map, height: '100%', width: '100%' }}
+      region={{
+        latitude: 49.263387,
+        longitude: -123.138176,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+    >
+      <Marker
+        coordinate={{ latitude: 49.263387, longitude: -123.138176 }}
+        title="RED Academy"
+        description="1490 W Broadway #200, Vancouver, BC V6H 4E8"
+        image={require('../../assets/images/map_pin.png')}
+      />
+    </MapView>
   );
 };
---
+++ js/screens/Map/styles.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
+export default styles;
