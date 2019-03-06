417251ac49164c327c40e1862acb055ea82fcef7
alexfts <alxfetisova@gmail.com>
2019-03-06 13:48:12 -0800

    Add no favourites message

--- js/screens/Faves/Faves.js
+++ js/screens/Faves/Faves.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import styles from '../../config/styles';
 import Events from '../../components/Events';
+import eventStyles from './styles';
 
 export default props => (
   <View styles={styles.Container}>
-    <Events {...props} />
+    {props.data && props.data.length > 0 ? (
+      <Events {...props} />
+    ) : (
+      <View styles={eventStyles.noFaves}>
+        <Text>No favourites</Text>
+      </View>
+    )}
   </View>
 );
---
+++ js/screens/Faves/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+  noFaves: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default styles;
